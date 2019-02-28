1c99958eda4484f80cac681505a3019188ecdcb9
syifasalsabyla228@gmail.com <syifasalsabyla228@gmail.com>
2019-02-28 17:34:19 +0700

    Add project files.

---
+++ direktoriMember/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace direktoriMember.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
